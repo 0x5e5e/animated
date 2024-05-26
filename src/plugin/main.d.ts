72b6ebd712f9b4e7493041b3080de7846f751d8c
Ben <ben@cambria.gg>
2024-05-26 11:52:57 +0700

    update types

--- src/plugin/main.d.ts
+++ src/plugin/main.d.ts
@@ -1,9 +1,9 @@
 export default class AnimatedTiles extends Phaser.Plugins.ScenePlugin {
-  init(map: Phaser.Tilemaps.Tilemap);
+  init(map: Phaser.Tilemaps.Tilemap, uuid: string);
   pause(layerIndex: number = null, mapIndex: number = null);
   resume(layerIndex: number = null, mapIndex: number = null);
   updateAnimatedTiles(x: number, y: number);
-  removeMap(map: Phaser.Tilemaps.Tilemap);
+  removeMap(uuid: string);
 
   setRate(rate: number, gid: number = null, map: number = null);
   resetRates(mapIndex: number = null);
